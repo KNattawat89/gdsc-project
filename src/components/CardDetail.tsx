ced37b70658370552a1939e01fb7c8b5ad17d02a
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-01-05 23:54:39 +0700

    some update on photo detail (sill undefined value from axios)

--- src/components/CardDetail.tsx
+++ src/components/CardDetail.tsx
@@ -1,30 +1,53 @@
-import { Card, CardActionArea, CardMedia, CardContent, Typography, Box } from '@mui/material'
-import React from 'react'
-import TurnedInIcon from '@mui/icons-material/TurnedIn';
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  Box,
+} from "@mui/material";
+import React, { FC } from "react";
+import TurnedInIcon from "@mui/icons-material/TurnedIn";
 
-const CardDetail = () => {
+const CardDetail: FC<{
+  title: string | undefined;
+  photoCount: number | undefined;
+  date: string | undefined;
+  savedPhoto: number | undefined;
+}> = ({ title, photoCount, date, savedPhoto }) => {
+  let eventdate;
+  if (date != undefined) {
+    const event = new Date(date);
+    eventdate =
+      event.toLocaleString("default", { month: "short" }) +
+      " " +
+      event.getDate() +
+      ", " +
+      event.getFullYear();
+  }
+
   return (
     <Card sx={{ maxWidth: 345 }}>
-    <CardActionArea>
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{ color: "#5D7FD8", fontWeight: "bold" }}
-        >
-          Lizard
-        </Typography>
-        <Typography variant="body2">123 Photos</Typography>
-        <Typography variant="body2">Date</Typography>
-        <Box sx={{display:"flex", marginTop:"1rem"}}>
-            <TurnedInIcon sx={{color:"#637196"}}/>
-            <Typography>18 Saved</Typography>
-        </Box>
-      </CardContent>
-    </CardActionArea>
-  </Card>
-  )
-}
+      <CardActionArea>
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{ color: "#5D7FD8", fontWeight: "bold" }}
+          >
+            {title}
+          </Typography>
+          <Typography variant="body2">{photoCount} Photos</Typography>
+          <Typography variant="body2">{eventdate}</Typography>
+          <Box sx={{ display: "flex", marginTop: "1rem" }}>
+            <TurnedInIcon sx={{ color: "#637196" }} />
+            <Typography>{savedPhoto} Saved</Typography>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
 
-export default CardDetail
+export default CardDetail;
--- src/pages/gallery/photoDetail.tsx
+++ src/pages/gallery/photoDetail.tsx
@@ -1,29 +1,69 @@
 import { Box, Container, Grid, ImageList, ImageListItem } from "@mui/material";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import CardDetail from "../../components/CardDetail";
 import Navbar from "../../components/Navbar";
 import PhotoCard from "../../components/PhotoCard";
 
-const photoDetail = () => {
+export function axiosGet() {
+  return axios
+    .get("http://server2.bsthun.com:4480/mock/gallery-detail.json")
+    .then((res) => {
+      return res.data.data;
+    })
+    .catch((err) => console.log(err));
+}
+
+const PhotoDetail = () => {
+  const params = useParams();
+  const [data, setData] = useState<{
+    albums: {
+      name: string;
+      photo_count: number;
+      saved_count: number;
+      date: string;
+      cover_photo: string;
+    };
+    photo: { thumbnail_url: string; full_url: string; date: string }[];
+  }>();
+  const getData = async () => {
+    const res = await axios.get(
+      "http://server2.bsthun.com:4480/mock/gallery-detail.json"
+    );
+    setData(res.data.data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
+
   return (
     <Navbar>
       <Container sx={{ marginTop: "3rem", width: "100%" }}>
         <Box
           sx={{
             display: "grid",
-            gridTemplateColumns: {sm:"1fr 2fr"},
-            columnGap: {sm:"1rem"},
-            rowGap:{xs:"1rem"},
+            gridTemplateColumns: { sm: "1fr 2fr" },
+            columnGap: { sm: "1rem" },
+            rowGap: { xs: "1rem" },
           }}
         >
           <Box>
-            <CardDetail />
+            {data ? (
+              <CardDetail
+                title={data?.albums?.name}
+                date={data?.albums?.date}
+                photoCount={data?.albums?.photo_count}
+                savedPhoto={data?.albums?.saved_count}
+              />
+            ) : null}
           </Box>
           <Box>
             <Grid container justifyContent="center">
-              <Grid item md={4} >
+              <Grid item md={4}>
                 <img
-                    style={{borderRadius:"1rem"}}
+                  style={{ borderRadius: "1rem" }}
                   width="220"
                   src="https://images.unsplash.com/photo-1672680587729-79346d3a1857?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
                 />
@@ -61,7 +101,7 @@ const photoDetail = () => {
             <img
               width="320"
               src="https://images.unsplash.com/photo-1672843386042-ec530be0a5f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-            /> 
+            />
           </Box>
         </Box>
       </Container>
@@ -69,4 +109,4 @@ const photoDetail = () => {
   );
 };
 
-export default photoDetail;
+export default PhotoDetail;
