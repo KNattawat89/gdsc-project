16a18c02580cb366a94787f4d47648b4af7f32cf
Nattawat Teaiwtan <Nao.Nait@gmail.com>
2023-01-17 11:19:18 +0700

    [Update] link to albums page in Gallery page

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -2,6 +2,7 @@ import { Box, Stack, Typography } from '@mui/material';
 import React from 'react';
 import  img  from "./img/wind.png";
 import img2 from "./img/Google.png";
+import { Link } from 'react-router-dom';
 const index = () => {
   return (
     <div style={{backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat', backgroundSize: "cover", width:"100vw", height:"100vh"}} >
@@ -19,10 +20,12 @@ const index = () => {
     
       <Stack direction="column" sx={{backgroundColor: "#00000080"}} width={{xs: "23rem",sm:"25rem"}} height="14.9rem" borderRadius="12px" mr={{xs: "0",md:"5.2rem"}} alignItems={"center"}>
        <Typography fontFamily="body2" fontWeight="600" mt="3.9rem" mb="1.5rem"> To getting started... </Typography>
+       <Link to={"/gallery/albums"} style={{textDecoration: "none", color: "white"}}>
        <Box width="20.5rem" height="3.75rem" borderRadius="12px" border="2px solid white" display="flex" alignItems="center">
          <img src={img2} width="64px" height="32px" style={{marginLeft: "1rem", marginRight: "0.5rem"}}/>
-         <Typography fontFamily="body2" fontWeight="600" > Sign in with Google </Typography>
+         <Typography fontFamily="body2" fontWeight="600"> Sign in with Google </Typography>
        </Box>
+       </Link>
       </Stack>
 
     </Stack>
